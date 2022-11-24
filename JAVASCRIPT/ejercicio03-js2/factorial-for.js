9c6c0a1e46efe8e7db131f4c262ea085e9279cc1
Agustina <agus.gambarte@gmail.com>
2022-11-24 15:43:50 -0300

    Realizado

--- JAVASCRIPT/ejercicio03-js2/factorial-for.js
+++ JAVASCRIPT/ejercicio03-js2/factorial-for.js
@@ -5,6 +5,6 @@ var factorial = 1;
 
 for (let i = 10; i > 0; i--) {
     factorial = factorial * i;
-}
+};
 
 console.log(factorial);
--- JAVASCRIPT/ejercicio03-js2/factorial-while.js
+++ JAVASCRIPT/ejercicio03-js2/factorial-while.js
@@ -5,6 +5,6 @@ let num = 10;
 while (num > 1) {
     factorial *= num;
     num--;
-}
+};
 
 console.log(factorial);
