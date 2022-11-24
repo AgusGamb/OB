ed405d89920270d037b80ae1e36b8108b7166b2f
Agustina <agus.gambarte@gmail.com>
2022-11-24 15:43:20 -0300

    Realizado

--- JAVASCRIPT/ejercicio03-js2/factorial-for.js
+++ JAVASCRIPT/ejercicio03-js2/factorial-for.js
@@ -1,9 +1,10 @@
 // factorial de 10 = 10*9*8*7*6*5*4*3*2*1
 
 
-var num = 10;
+var factorial = 1;
 
-for (let i = 1; i <= num; i++) {
-    num = num * i;
-    console.log(num);
+for (let i = 10; i > 0; i--) {
+    factorial = factorial * i;
 }
+
+console.log(factorial);
