2175d9e4d07270fd25568c428efd1061c4e58f3c
Agustina <agus.gambarte@gmail.com>
2022-11-25 17:26:16 -0300

    Realizado. Math ceil-floor-MAX_VALUE

---
+++ JAVASCRIPT/ejercicio05/numeros.js
@@ -0,0 +1,28 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una variable que contenga tu altura en centímetros (entero)
+
+let altura_cm = 167;
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+
+let altura_mts = 1.67;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+let peso_kg = 61.5;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let altura_ceil = Math.ceil(altura_mts);
+console.log(altura_round)
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+let peso_floor = Math.floor(peso_kg);
+console.log(peso_floor);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+let iguales = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(iguales)
