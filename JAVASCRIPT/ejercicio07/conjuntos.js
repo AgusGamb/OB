9595bb858fa68605d01ba78898f7748fbc655f12
Agustina <agus.gambarte@gmail.com>
2022-12-05 17:33:26 -0300

    SETS .add

---
+++ JAVASCRIPT/ejercicio07/conjuntos.js
@@ -0,0 +1,15 @@
+// SETS
+
+//Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+//- Un nuevo Set con los nombres de tu familia
+
+let setFamila = new Set(['Facundo', 'Gonzalo', 'Julieta']);
+
+//- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+setFamila.add('Agustina');
+
+//- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+
+setFamila.add('JavaScript');
+console.log(setFamila)
