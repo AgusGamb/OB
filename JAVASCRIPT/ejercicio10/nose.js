7f0b52ac1dbce734ce94df62dfff5790aac402a7
Agustina <agus.gambarte@gmail.com>
2024-03-13 19:47:17 -0300

    nte e rregr compu

---
+++ JAVASCRIPT/ejercicio10/nose.js
@@ -0,0 +1,14 @@
+var testArray = ['test'];
+function testFunc() {
+    for (var i = 0; i < testArray.length; i++) {
+        console.log(testArray[i])
+    }
+}
+testFunc();
+
+
+const myName = 'Julieta'
+if(myName) {
+    myName ='pedro'
+    console.log(myName)
+}
